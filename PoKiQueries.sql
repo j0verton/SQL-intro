0480c6e376552d0da002f950b21893dc492e873c
Joe <overtonje@gmail.com>
2020-11-24 12:52:55 -0600

    almost have 15 cant query author nae

--- PoKiQueries.sql
+++ PoKiQueries.sql
@@ -86,15 +86,24 @@ WHERE WordCount = (
 	FROM POEM);
 
 --#15
---broke!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-SELECT p.Id, a.Name
-FROM Poem p
-INNER JOIN Author a ON a.Id = p.AuthorId
-WHERE p.Id = (
-	SELECT COUNT(p.Id) as NumberOfPoems
-	FROM POEM)
-GROUP BY p.AuthorId
-ORDER BY p.Id
+--cant run if also query author name!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+--which author has the most poems
+
+SELECT Author.Name, MAX(Author.Num)
+FROM (SELECT COUNT(Id) AS Num
+	FROM Poem
+	group by Authorid
+) Author;
+
+
+--SELECT p.Id, a.Name
+--FROM Poem p
+--INNER JOIN Author a ON a.Id = p.AuthorId
+--WHERE p.Id = (
+--	SELECT COUNT(p.Id) as NumberOfPoems
+--	FROM POEM)
+--GROUP BY p.AuthorId
+--ORDER BY p.Id
 
 --#16
 SELECT COUNT (p.Id) AS SadPoemCount
@@ -104,22 +113,33 @@ INNER JOIN Emotion e ON e.Id = pe.EmotionId
 WHERE e.Name = 'Sadness'
 
 --#17
-
-
+--how many poems have no emotion
+--broke!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 SELECT pe.PoemId, p.Title
 FROM Poem p
 LEFT JOIN PoemEmotion pe ON pe.PoemId = p.Id
 --INNER JOIN Emotion e ON e.Id = pe.EmotionId
 WHERE pe.PoemId = (
-SELECT COUNT (pe.PoemId) AS EmotionCount
-  = 0
-)
+	SELECT COUNT (pe.PoemId) AS EmotionCount)
 GROUP BY p.Title 
 
 
 SELECT * 
 FROM PoemEmotion
 ORDER BY PoemId
+
 --#18
+--find emotion with fewest poems
+--broke!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+--SELECT MIN (poemCount)
+--FROM (SELECT p.Id, COUNT(p.Id) poemCount
+--	FROM Poem
+--	INNER JOIN PoemEmotion pe ON pe.PoemId = p.Id
+--	INNER JOIN Emotion e ON e.Id = pe.EmotionId
+--	GROUP BY e.Name);
+
+
 --#19
+
 --#20
