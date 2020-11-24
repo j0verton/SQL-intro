fa9fe272976a323bb5a67d7946a5a042e4994c58
Joe <overtonje@gmail.com>
2020-11-24 10:46:47 -0600

    POKI

---
+++ PoKiQueries.sql
@@ -0,0 +1,43 @@
+﻿--#1
+SELECT id, name
+FROM GRADE
+
+--#2
+SELECT id, name
+FROM Emotion
+
+--#3
+SELECT COUNT(Id) AS NumberOfPoems
+FROM Poem
+
+--#4
+SELECT Id, Name
+FROM Author
+WHERE Id < 77
+--WHERE Id BETWEEN 0 AND 76
+ORDER BY Name
+
+--#5
+SELECT a.Name, g.Name
+FROM Author a
+INNER JOIN Grade g ON g.id = a.GradeId
+WHERE a.Id < 77
+ORDER BY a.Name
+
+
+--#6
+SELECT a.Name, g.Name, ge.Name
+FROM Author a
+INNER JOIN Grade g ON g.id = a.GradeId
+INNER JOIN Gender ge ON ge.id = a.GenderId
+WHERE a.Id < 77
+ORDER BY a.Name
+
+--#7
+SELECT SUM(WordCount) AS TotalWords
+FROM Poem
+
+--#8
+SELECT MIN(WordCount) AS ShortestPoem, Title
+FROM Poem
+--WHERE MIN(WordCount)
